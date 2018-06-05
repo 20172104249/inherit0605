d4e06a29c57ef96460f2c586261c179e77e707b0
20172104249 <2980796841@qq.com>
2018-06-05 15:41:13 +0800

    Initial commit

---
+++ inherit0605/inherit0605.cpp
@@ -0,0 +1,67 @@
+// inherit0605.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+class CA
+{
+protected:
+	int a;
+	int b;
+public:
+	CA()
+	{
+		a = 1;
+		b = 2;
+		cout << "a=" << a << "b=" << b << endl;
+	}
+	CA(int a, int b)
+	{
+		cout << "a=" << a << "b=" << b << endl;
+	}
+	~CA()
+	{
+		cout << "析构" << endl;
+	}
+};
+class CB
+{
+protected:
+	int c;
+	int d;
+public:
+	CB()
+	{
+		c = 2;
+		d = 1;
+		cout << "c=" << c << "d=" << d << endl;
+	}
+	CB(int c, int d)
+	{
+		cout << "c=" << c << "d=" << d << endl;
+	}
+	~CB()
+	{
+		cout << "析构1" << endl;
+	}
+};
+class CC :public CB, CA
+{
+public:
+	CC(int g, int h):CB(g,h)
+	{
+		cout << a<<" " <<b<<" "<<g<<" " << h << endl;
+	}
+	~CC()
+	{
+		cout << "析构2" << endl;
+	}
+};
+
+
+int main()
+{
+	CC o(5, 6);
+	return 0;
+}
